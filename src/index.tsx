de5d6beb1ade4a3247dd98562f2e3aed25dca528
slimemoss <common.tken@gmail.com>
2023-03-09 17:47:55 +0900

    Tabでは05の検索がうまくいかないので、02だけに戻す

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import {Tab, Tabs} from 'react-bootstrap'
 
 import { CardSearch02Page } from './card_search/CardSearch02'
-import { CardSearch05Page } from './card_search/CardSearch02'
 
 ReactDOM.render(
   <>
-    <Tabs
-      defaultActiveKey="s02"
-      id="tab"
-      className="mb-3"
-    >
-      <Tab eventKey="s02" title="02環境">
-        <CardSearch02Page />
-      </Tab>
-      <Tab eventKey="s05" title="05環境">
-        <CardSearch05Page />
-      </Tab>
-    </Tabs>
+    <CardSearch02Page/>
   </>,
   document.getElementById('app')
 )
